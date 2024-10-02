5127ffb6173f47d52da4296e906e7f42e6e500ab
unknown <belikehao@gmail.com>
2024-10-02 15:39:55 +0900

    1

---
+++ Test01/Test01/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Test01.cpp
+ *
+ * Created: 2024-10-02 오후 12:17:31
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
