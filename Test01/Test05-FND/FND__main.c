ada03ef9a5c63bbf6b21973517d5dc093e62d5fc
unknown <belikehao@gmail.com>
2024-10-18 16:54:06 +0900

    24_10_18

--- Test01/Test05-FND/FND__main.c
+++ Test01/Test05-FND/FND__main.c
@@ -1,115 +1,258 @@
-#define F_CPU 16000000
+	#define F_CPU 16000000
 
-#include <avr/io.h>
-#include <avr/delay.h>
-#include <avr/interrupt.h>
+	#include <avr/io.h>
+	#include <avr/delay.h>
+	#include <avr/interrupt.h>
 
-#define CDDR	DDRA
-#define IDDR	DDRB
-#define CPORT	PORTA
-#define IPORT	PORTB
+	#define CDDR	DDRA
+	#define IDDR	DDRB
+	#define CPORT	PORTA
+	#define IPORT	PORTB
 
-unsigned char img[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x27, 0x7f, 0x67};
-char digit[4] = {0};
+	unsigned char img[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x27, 0x7f, 0x67};
+
+	char digit_0[4] = {0};
+	char digit_1[4] = {0};
 	
-volatile int num = 0;	
-volatile char btn =0;
+	volatile int num = 0;
+	volatile int timee = 0;
+	volatile char btn_0 = 0;
+	volatile char btn_1 = 0;
+	volatile int clock_mode = 0;
+	volatile int buzzer = 0;
 	
-int sep (int* a)
-{	if(a>9999) 
-	{
-		*a = 0;
-		return 0;
-	}
-	else
-	{
-		digit[3] = (char)((*a%10) / 1);
-		digit[2] = (char)((*a%100) / 10);
-		digit[1] = (char)((*a%1000) / 100);
-		digit[0] = (char)((*a%10000) / 1000);
-	}
+	/*
+	자리수 분리
+	*/
+	
+	int sep_0 (int* a)
+	{	if(a>9999)
+		{
+			*a = 0;
+			return 0;
+		}
+		else
+		{
+			digit_0[3] = (char)((*a%10) / 1);
+			digit_0[2] = (char)((*a%100) / 10);
+			digit_0[1] = (char)((*a%1000) / 100);
+			digit_0[0] = (char)((*a%10000) / 1000);
+		}
 		return 1;
-}
+	}
+
+	int sep_1 (int* a)
+	{
+		if(a>3600)
+		{
+			*a = 0;
+			return 0;
+		}
+		else
+		{
+			int min = (*a)%60;
+			int hour = (*a/60)%60;
+			digit_1[3] = (char)((min%10) / 1);
+			digit_1[2] = (char)((min%100) / 10);
+			digit_1[1] = (char)((hour%10) / 1);
+			digit_1[0] = (char)((hour%100) / 10);
+		}
+		return 1;
+	}
+
+	/*
+	FND refresh 기능
+	*/
+
+	ISR(TIMER2_OVF_vect) // FND refresh
+	{
+		//타이머 동작시 FND refresh
+		if (clock_mode == 1)
+		{
+			
+			for(int i =0; i<4; i++)
+			{
+				CPORT = 1<< i;
+				if(i!=1)
+				{
+					IPORT = ~img[digit_0[i]];
+					_delay_ms(3);
+				}
+				if(i ==1)
+				{
+					IPORT = ~(img[digit_0[i]]|0x80);
+					_delay_ms(3);
+				}
+				
+			}
+		}
+		// 시계 동작시  FND refresh
+		else if(clock_mode == 0)
+		{
+			for(int i =0; i<4; i++)
+			{
+				CPORT = 1<< i;
+				if(i!=1)
+				{
+					IPORT = ~img[digit_1[i]];
+					_delay_ms(3);
+				}
+				if(i ==1)
+				{
+					IPORT = ~(img[digit_1[i]]|0x80);
+					_delay_ms(3);
+				}
+				
+			}
+		}
+		
+	}
 	
-
-
-ISR(TIMER0_COMP_vect)
-{
-		num++;
-		if(num>10000)
+	/*
+	Stop watch 기능
+	btn_0 => INT 0 => 두번째 노란색 버튼
+	digit_0 사용
+	*/
+	ISR(TIMER0_COMP_vect)
+	{
+		num--;
+		if(num>10000 || num < 0)
 		{
 			num = 0;
-		}
-	
-}
-
-
-ISR(TIMER2_OVF_vect) // FND refresh
-{
-	for(int i =0; i<4; i++)
-	{
-		CPORT = 1<< i;
-		if(i!=1)
-		{
-			IPORT = ~img[digit[i]];
-			_delay_ms(3);
-		}
-		if(i ==1)
-		{
-			IPORT = ~(img[digit[i]]|0x80);
-			_delay_ms(3);
+			TIMSK &= ~0x02;
+			btn_0 = 0;
+			PORTG |= 0x08; // buzzer on
 		}
 		
 	}
-}
 
-ISR(INT0_vect)
-{
-	if(btn == 0)
-	{
-	num =0;
-	btn++;	
-	}
-	else if(btn == 1)
-	{
-	TIMSK |= 0x02; // OCIE0
-	btn++;
-	}
-	else
-	{
-	TIMSK &= ~0x02;
-	btn = 0;
-	}
 
-	
-}
-	
-int main(void)
-{
-	//Timer0: 100ms stop-watch, timer2: FND refresh
+	ISR(INT0_vect)
+	{
+		switch(btn_0)
+		{
+		case 0:
+			PORTG &= ~0x08;//buzzer off
+			btn_0++;
+			break;
 			
-	//TIMSK |= 0x02; // OCIE0
-	TIMSK |= 0x40; // 0b 01xx xxxx TOIE2
-	TCCR0 |= 0x07; // 분주비 1024  1/16M * 1024 *156
-	TCCR2 |= 0x05; // 분주비 110:256, (1/16M) * 1024 * 256 = 0.004096(sec)
-
-	
-	EICRA |= (1<<ISC01);    // INT0 하강엣지에서 인터럽트 발생
-	EIMSK |= (1<<INT0);     // INT0 인터럽트 활성화
 		
-	OCR0 = 156; //(1/16M) * 156 * 1024 *  = 0.099904 (~= 100ms)
+		case 1: // 타이머 설정 모드 (초)
+				
+			TIMSK &= ~0x02;
+			btn_0++;
+			break;
+		
+		case 2: // 타이머 start 모드 
+		
+			TIMSK |= 0x02; // OCIE0
+			break;
+		}
 
-	CDDR |= 0x0f; // 하위 4bit을 출력으로 설정
-	IDDR |= 0xff; // 8bit 전체를 출력으로 설정
-	
-	CPORT = 0x0f; //모든 자리수 선택
-	IPORT = ~0xff; //전체 세그먼트 ON ---> 8.
+		
+	}
 
+	/*
+	시계 기능 구현 (16비트 타이머 <1번 카운터>)
+	btn_1 => INT 1 => 첫번째 노란색 버튼 clock_mode 변경
+	btn_3 => INT 2 => 세번째 노란색 버튼 timee += 60;
+	*/
 	
-	sei();	
-	while (1) 
-    {
-		sep(&num);
-    }
-}
+	ISR(INT1_vect)
+	{
+		switch (clock_mode)
+		{
+			case 0:
+				clock_mode = 1;
+
+				break;
+			
+			case 1:
+				clock_mode = 0;
+
+				break;
+			
+		}
+	}
+
+	ISR(INT2_vect)
+	{
+		
+		switch (clock_mode)
+		{
+			case 0:
+					timee += 60;
+					break;
+
+
+			case 1:
+				if (btn_0 == 1)
+				{
+					num += 100;
+				}
+
+				break;
+					
+		}
+		
+	}
+
+
+
+	ISR(TIMER1_COMPA_vect)
+	{
+		timee++;
+		if(timee>3600)
+		{
+			timee = 0;
+		}
+		
+	}
+	
+	int main(void)
+	{
+		//Timer0: 100ms stop-watch, timer2: FND refresh
+		
+		//TIMSK |= 0x02; // OCIE0
+		TIMSK |= 0x42; // 0b 01xx_xx1x 0번 카운터 비교일치 : 1, 2번 카운터 오버플로 : 1
+		TIMSK |= 0x10; // 0b xxx1_xxxx 1번 카운터 비교일치 : 1
+		
+		TCCR0 |= 0x0f; // 분주비 1024  1/16M * 1024 *156
+		TCCR2 |= 0x05; // 분주비 111:1024, (1/16M) * 1024 * 256 = 0.004096(sec)
+		
+		TCCR1A =0;
+		
+		TCCR1B |=0x0d; // 분주비 101:1, (1/16M) * 1024 * 15625 = 1(sec)
+		 //(WGM13, WGM12 on) CTC  mode 동작
+		
+		
+		EIMSK |= 0x07;     // INT0,1,2 인터럽트 활성화
+		EICRA |= 0x3a;    // INT0,1,2 하강엣지에서 인터럽트 발생
+		
+		OCR0 = 156; //(1/16M) * 156 * 1024 *  = 0.099904 (~= 100ms)
+		OCR1A = 15625;
+		
+		CDDR |= 0x0f; // 하위 4bit을 출력으로 설정
+		IDDR |= 0xff; // 8bit 전체를 출력으로 설정
+		
+		CPORT = 0x0f; //모든 자리수 선택
+		IPORT = ~0xff; //전체 세그먼트 ON ---> 8.
+		
+		DDRG |= 0x08; //PORTG 3번 출력 설정
+		
+		
+		btn_0 = 0;
+		btn_1 = 0;
+		clock_mode = 0;
+		sei();
+		while (1)
+		{
+			sep_0(&num);
+			sep_1(&timee);
+			
+
+		}
+	}
+
+
 
---
+++ d
@@ -0,0 +1,60 @@
+[33mcommit 7ddc6e04cccc9b6205621bbf858b8539b1a96b6d[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m)[m
+Author: unknown <belikehao@gmail.com>
+Date:   Thu Oct 17 16:17:30 2024 +0900
+
+    24_10_27_atmega128
+
+[33mcommit 0ef1346b51b119c62206b6597267239ef7912212[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: unknown <belikehao@gmail.com>
+Date:   Wed Oct 2 16:53:15 2024 +0900
+
+    3
+
+[33mcommit 6045b61edb1cf2d595275e628f15aaab5d7f23c6[m
+Merge: 5127ffb 5c2b462
+Author: unknown <belikehao@gmail.com>
+Date:   Wed Oct 2 16:19:44 2024 +0900
+
+    1
+
+[33mcommit 5c2b4627cc0c01b4785b6dd1d219d100c3131692[m
+Author: hao-jong <belikehao@gmail.com>
+Date:   Wed Oct 2 16:10:16 2024 +0900
+
+    Update .gitignore
+
+[33mcommit 53b2014c8b995b44bf6ce58572a09bbf64b550e2[m
+Author: hao-jong <belikehao@gmail.com>
+Date:   Wed Oct 2 15:44:10 2024 +0900
+
+    Update .gitignore
+
+[33mcommit 5127ffb6173f47d52da4296e906e7f42e6e500ab[m
+Author: unknown <belikehao@gmail.com>
+Date:   Wed Oct 2 15:39:55 2024 +0900
+
+    1
+
+[33mcommit 3a8b82ab63901fa29f78d9b5f37617d0e1036da4[m
+Author: hao-jong <belikehao@gmail.com>
+Date:   Wed Oct 2 14:42:00 2024 +0900
+
+    Update README.md
+
+[33mcommit 4845170ebd73adf33a6eb6cc103a6f31bdc830c5[m
+Author: hao-jong <belikehao@gmail.com>
+Date:   Wed Oct 2 14:36:52 2024 +0900
+
+    Update README.md
+
+[33mcommit 84b3070dd65789f74151055dddf1c651c81e294b[m
+Author: hao-jong <belikehao@gmail.com>
+Date:   Wed Oct 2 14:36:25 2024 +0900
+
+    Update README.md
+
+[33mcommit b1470ff6f60afb7c635d9495df490c16c9a60d81[m
+Author: hao-jong <belikehao@gmail.com>
+Date:   Wed Oct 2 14:34:30 2024 +0900
+
+    Initial commit
